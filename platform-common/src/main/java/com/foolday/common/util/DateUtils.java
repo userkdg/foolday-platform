2478250e5c735271fda40d87f60c1a1ab2ae3d82
kindg <1265598443@qq.com>
2019-04-03 01:18:32 +0800

    fix

--- platform-common/src/main/java/com/foolday/common/util/DateUtils.java
+++ platform-common/src/main/java/com/foolday/common/util/DateUtils.java
@@ -1,12 +1,29 @@
 package com.foolday.common.util;
 
+import com.google.common.collect.Lists;
 import org.apache.commons.lang3.time.DateFormatUtils;
 
 import javax.validation.constraints.NotNull;
 import java.util.Date;
+import java.util.List;
 
 public class DateUtils {
-    public static String formatStandard(@NotNull Date date) {
-        return DateFormatUtils.format(date, "yyyy-MM-dd HH:mm:ss");
+    private static final List<String> datePattern = Lists.newArrayList();
+
+    public static final String yyyy_MM_dd_HH_mm_ss = "yyyy-MM-dd HH:mm:ss";
+    public static final String yyyy_MM_dd_hh_mm_ss = "yyyy-MM-dd hh:mm:ss";
+
+    static {
+        datePattern.add(yyyy_MM_dd_HH_mm_ss);
+        datePattern.add(yyyy_MM_dd_hh_mm_ss);
     }
+
+
+    private DateUtils() {
+    }
+
+    public static String formatStandard(@NotNull Date date) {
+        return DateFormatUtils.format(date, yyyy_MM_dd_HH_mm_ss);
+    }
+
 }
