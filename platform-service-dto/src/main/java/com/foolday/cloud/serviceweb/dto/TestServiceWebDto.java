089a79d17f41089e1bb76b32e3cecde8fb25ec0d
kindg <1265598443@qq.com>
2019-04-06 16:20:34 +0800

    增加swagger2及其部分用法

--- platform-service-dto/src/main/java/com/foolday/cloud/serviceweb/dto/TestServiceWebDto.java
+++ platform-service-dto/src/main/java/com/foolday/cloud/serviceweb/dto/TestServiceWebDto.java
@@ -1,5 +1,6 @@
 package com.foolday.cloud.serviceweb.dto;
 
+import io.swagger.annotations.ApiModel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,6 +10,7 @@ import java.io.Serializable;
 @Setter
 @Getter
 @ToString
+@ApiModel("服务层和web层交互实体")
 public class TestServiceWebDto implements Serializable {
     private String id;
     private String name;
