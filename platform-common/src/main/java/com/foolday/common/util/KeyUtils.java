aaf6c04fb6c0585bc618440cdf4d0190d76058dc
kindg <1265598443@qq.com>
2019-04-25 21:02:53 +0800

    1.增加订单号生成（基于redis)

--- platform-common/src/main/java/com/foolday/common/util/KeyUtils.java
+++ platform-common/src/main/java/com/foolday/common/util/KeyUtils.java
@@ -25,24 +25,25 @@ public final class KeyUtils {
     /**
      * 生成每天的唯一订单号
      * <p>
-     * 201904250133200001
-     * 201904250133200002
-     * 201904250133200003
-     * 201904250133200004
-     * 201904250133200005
+     * 20190425000000001
+     * 20190425000000002
+     * 20190425000000003
+     * 20190425000000004
+     * 20190425000000005
+     * 20190425000000006
      *
      * @param redisTemplate
      * @return
      */
     public static String generateOrderNo(RedisTemplate<String, String> redisTemplate, LocalDateTime dateTime) {
         PlatformAssert.notNull(redisTemplate, "redis 客户端不可为空");
-        String yyyyMMddHHmmss = dateTime.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
-        if (Boolean.TRUE.equals(redisTemplate.hasKey(yyyyMMddHHmmss))) {
-            redisTemplate.opsForValue().increment(yyyyMMddHHmmss, 1L);
-            return redisTemplate.opsForValue().get(yyyyMMddHHmmss);
+        String yyyyMMdd = dateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        if (Boolean.TRUE.equals(redisTemplate.hasKey(yyyyMMdd))) {
+            redisTemplate.opsForValue().increment(yyyyMMdd, 1L);
+            return redisTemplate.opsForValue().get(yyyyMMdd);
         } else {
-            String dayFirstNo = (yyyyMMddHHmmss + "000" + 1);
-            redisTemplate.opsForValue().set(yyyyMMddHHmmss, dayFirstNo, 31L, TimeUnit.DAYS);
+            String dayFirstNo = (yyyyMMdd + "00000000" + 1);
+            redisTemplate.opsForValue().set(yyyyMMdd, dayFirstNo, 31L, TimeUnit.DAYS);
             return dayFirstNo;
         }
     }
