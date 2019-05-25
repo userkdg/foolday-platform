256b8a755e3daf4298637e0c5c4e7594b2e8d2e8
userkdg <1265598443@qq.com>
2019-05-25 15:22:12 +0800

    1.fix
    Signed-off-by: userkdg <1265598443@qq.com>

--- platform-dao/src/main/java/com/foolday/dao/systemUrl/SystemUrlEntity.java
+++ platform-dao/src/main/java/com/foolday/dao/systemUrl/SystemUrlEntity.java
@@ -4,25 +4,27 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import com.foolday.common.base.BaseEntity;
 import com.foolday.common.enums.CommonStatus;
 import com.foolday.common.enums.HttpMethodType;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.util.Objects;
 
 /**
  * id varchar(36) not null,
- * 	url varchar(500) not null comment '控制器的url',
- * 	auth_http_method tinyint(2) null comment '请求方式 httpMethod',
- * 	create_time datetime default CURRENT_TIMESTAMP null,
- * 	update_time datetime null on update CURRENT_TIMESTAMP,
- * 	status tinyint(2) null comment 'uri是否有效',
- * 	base_url varchar(100) null comment '基础uri eg：/system || /weChat if null get url else base_url+url'
+ * url varchar(500) not null comment '控制器的url',
+ * auth_http_method tinyint(2) null comment '请求方式 httpMethod',
+ * create_time datetime default CURRENT_TIMESTAMP null,
+ * update_time datetime null on update CURRENT_TIMESTAMP,
+ * status tinyint(2) null comment 'uri是否有效',
+ * base_url varchar(100) null comment '基础uri eg：/system || /weChat if null get url else base_url+url'
  *
  * @author userkdg
  * @date 2019/5/25 12:10
  **/
 @TableName("t_system_url")
-@Data
+@Setter
+@Getter
 @ToString(callSuper = true)
 public class SystemUrlEntity extends BaseEntity<SystemUrlEntity> {
     private String url;
