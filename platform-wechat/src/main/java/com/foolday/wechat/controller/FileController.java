ea88342fd4b55a236930639ac70eba9112313e83
userkdg <1265598443@qq.com>
2019-06-09 15:20:33 +0800

    fix bug ssss

--- platform-wechat/src/main/java/com/foolday/wechat/controller/FileController.java
+++ platform-wechat/src/main/java/com/foolday/wechat/controller/FileController.java
@@ -1,23 +1,28 @@
 package com.foolday.wechat.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.foolday.common.dto.FantResult;
 import com.foolday.common.exception.PlatformException;
+import com.foolday.dao.image.ImageEntity;
 import com.foolday.service.api.base.Image2DiskServiceApi;
 import com.foolday.wechat.base.MultipartFileUtils;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
-@Api(value = "文件上传",tags = "文件上传")
+@Api(value = "文件上传", tags = "文件上传")
 @RestController
 @RequestMapping("/file")
 public class FileController {
@@ -44,6 +49,29 @@ public class FileController {
     }
 
 
+    @ApiOperation(value = "查看图片")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "正常返回", response = ResponseEntity.class)})
+    @GetMapping("/viewImage/size")
+    public ResponseEntity<FileSystemResource> view(@ApiParam("原文件id") @RequestParam("id") String id,
+                                                   @ApiParam("宽度(px)") @RequestParam("width") Integer width,
+                                                   @ApiParam("高度(px)") @RequestParam("height") Integer height) {
+        File file = image2DiskServiceApi.getThumbnail(id, width, height).getFile();
+        if (file == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .cacheControl(CacheControl.maxAge(1, TimeUnit.DAYS))
+                .body(new FileSystemResource(file));
+
+    }
+
+    @ApiOperation("获取图片 list")
+    @GetMapping("/list")
+    public FantResult<List<ImageEntity>> list() {
+        @SuppressWarnings("unchecked")
+        LambdaQueryWrapper<ImageEntity> eq = image2DiskServiceApi.lqWrapper()
+                .orderByDesc(ImageEntity::getType, ImageEntity::getUpdateTime);
+        List<ImageEntity> entity = image2DiskServiceApi.selectList(eq);
+        return FantResult.ok(entity);
+    }
 }
 
 
